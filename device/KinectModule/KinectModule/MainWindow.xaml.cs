9a3e0e4fc5882ea35a8ce9beda2ce8b7f68b910e
Ramik Sadana <sadana.ramik@gmail.com>
2013-02-13 15:26:40 -0500

    Added Kinect module

---
+++ device/KinectModule/KinectModule/MainWindow.xaml.cs
@@ -0,0 +1,421 @@
+﻿using System.Windows;
+using System.Windows.Data;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+using Microsoft.Samples.Kinect.WpfViewers;
+using System.Diagnostics;
+using System.ComponentModel;
+using System;
+using System.Net;
+using System.Text;
+using System.IO;
+using Fizbin.Kinect.Gestures;
+using Microsoft.Speech.Recognition;
+using System.Collections.Generic;
+using Microsoft.Speech.AudioFormat;
+
+namespace KinectModule
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private readonly KinectSensorChooser sensorChooser = new KinectSensorChooser();
+        private SpeechRecognitionEngine speechEngine;
+        private Skeleton[] skeletons = new Skeleton[0];
+
+        // skeleton gesture recognizer
+        private GestureController gestureController;
+
+        private List<System.Windows.Documents.Span> recognitionSpans;
+
+        public MainWindow()
+        {
+            DataContext = this;
+
+            InitializeComponent();
+
+            // initialize the Kinect sensor manager
+            KinectSensorManager = new KinectSensorManager();
+            KinectSensorManager.KinectSensorChanged += this.KinectSensorChanged;
+
+            // locate an available sensor
+            sensorChooser.Start();
+
+            // bind chooser's sensor value to the local sensor manager
+            var kinectSensorBinding = new Binding("Kinect") { Source = this.sensorChooser };
+            BindingOperations.SetBinding(this.KinectSensorManager, KinectSensorManager.KinectSensorProperty, kinectSensorBinding);
+
+            
+        }
+
+        #region Kinect Discovery & Setup
+
+        private void KinectSensorChanged(object sender, KinectSensorManagerEventArgs<KinectSensor> args)
+        {
+            if (null != args.OldValue)
+                UninitializeKinectServices(args.OldValue);
+
+            if (null != args.NewValue)
+                InitializeKinectServices(KinectSensorManager, args.NewValue);
+        }
+
+        /// <summary>
+        /// Kinect enabled apps should customize which Kinect services it initializes here.
+        /// </summary>
+        /// <param name="kinectSensorManager"></param>
+        /// <param name="sensor"></param>
+        private void InitializeKinectServices(KinectSensorManager kinectSensorManager, KinectSensor sensor)
+        {
+            // Application should enable all streams first.
+
+            // configure the color stream
+            kinectSensorManager.ColorFormat = ColorImageFormat.RgbResolution640x480Fps30;
+            kinectSensorManager.ColorStreamEnabled = true;
+
+            // configure the depth stream
+            kinectSensorManager.DepthStreamEnabled = true;
+
+            kinectSensorManager.TransformSmoothParameters =
+                new TransformSmoothParameters
+                {
+                    Smoothing = 0.5f,
+                    Correction = 0.5f,
+                    Prediction = 0.5f,
+                    JitterRadius = 0.05f,
+                    MaxDeviationRadius = 0.04f
+                };
+
+            // configure the skeleton stream
+            sensor.SkeletonFrameReady += OnSkeletonFrameReady;
+            kinectSensorManager.SkeletonStreamEnabled = true;
+
+            // initialize the gesture recognizer
+            gestureController = new GestureController();
+            gestureController.GestureRecognized += OnGestureRecognized;
+
+            kinectSensorManager.KinectSensorEnabled = true;
+
+            RecognizerInfo ri = GetKinectRecognizer();
+
+            if (null != ri)
+            {
+                // recognitionSpans = new List<Span> { forwardSpan, backSpan, rightSpan, leftSpan };
+
+                this.speechEngine = new SpeechRecognitionEngine(ri.Id);
+
+                var directions = new Choices();
+                directions.Add(new SemanticResultValue("forward", "FORWARD"));
+                directions.Add(new SemanticResultValue("forwards", "FORWARD"));
+                directions.Add(new SemanticResultValue("straight", "FORWARD"));
+                directions.Add(new SemanticResultValue("backward", "BACKWARD"));
+                directions.Add(new SemanticResultValue("backwards", "BACKWARD"));
+                directions.Add(new SemanticResultValue("back", "BACKWARD"));
+                directions.Add(new SemanticResultValue("turn left", "LEFT"));
+                directions.Add(new SemanticResultValue("turn right", "RIGHT"));
+                directions.Add(new SemanticResultValue("lights on", "LIGHT_ON"));
+                directions.Add(new SemanticResultValue("lights off", "LIGHT_OFF"));
+
+                var gb = new GrammarBuilder { Culture = ri.Culture };
+                gb.Append(directions);
+
+                var g = new Grammar(gb);
+
+                // Create a grammar from grammar definition XML file.
+                //using (var memoryStream = new MemoryStream(Encoding.ASCII.GetBytes(Properties.Resources.SpeechGrammar)))
+                //{
+                //    var g = new Grammar(memoryStream);
+                //    speechEngine.LoadGrammar(g);
+                //}
+
+                speechEngine.LoadGrammar(g);
+                speechEngine.SpeechRecognized += SpeechRecognized;
+                speechEngine.SpeechRecognitionRejected += SpeechRejected;
+
+                speechEngine.SetInputToAudioStream(sensor.AudioSource.Start(), new SpeechAudioFormatInfo(EncodingFormat.Pcm, 16000, 16, 1, 32000, 2, null));
+                speechEngine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            else
+            {
+                //this.statusBarText.Text = Properties.Resources.NoSpeechRecognizer;
+            }
+            if (!kinectSensorManager.KinectSensorAppConflict)
+            {
+                // addition configuration, as needed
+            }
+        }
+
+        /// <summary>
+        /// Kinect enabled apps should uninitialize all Kinect services that were initialized in InitializeKinectServices() here.
+        /// </summary>
+        /// <param name="sensor"></param>
+        private void UninitializeKinectServices(KinectSensor sensor)
+        {
+            if (null != sensor)
+            {
+                sensor.AudioSource.Stop();
+
+                sensor.Stop();
+                sensor = null;
+            }
+
+            if (null != this.speechEngine)
+            {
+                this.speechEngine.SpeechRecognized -= SpeechRecognized;
+                this.speechEngine.SpeechRecognitionRejected -= SpeechRejected;
+                this.speechEngine.RecognizeAsyncStop();
+            }
+        }
+
+
+        private static RecognizerInfo GetKinectRecognizer()
+        {
+            foreach (RecognizerInfo recognizer in SpeechRecognitionEngine.InstalledRecognizers())
+            {
+                string value;
+                recognizer.AdditionalInfo.TryGetValue("Kinect", out value);
+                if ("True".Equals(value, StringComparison.OrdinalIgnoreCase) && "en-US".Equals(recognizer.Culture.Name, StringComparison.OrdinalIgnoreCase))
+                {
+                    return recognizer;
+                }
+            }
+
+            return null;
+        }
+
+        private void ClearRecognitionHighlights()
+        {
+            
+        }
+
+        /// <summary>
+        /// Handler for recognized speech events.
+        /// </summary>
+        /// <param name="sender">object sending the event.</param>
+        /// <param name="e">event arguments.</param>
+        private void SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            // Speech utterance confidence below which we treat speech as if it hadn't been heard
+            const double ConfidenceThreshold = 0.3;
+
+            ClearRecognitionHighlights();
+                Debug.WriteLine(e.Result.Semantics.Value.ToString());
+
+            if (e.Result.Confidence >= ConfidenceThreshold)
+            {
+                switch (e.Result.Semantics.Value.ToString())
+                {
+                    case "FORWARD":
+                        break;
+
+                    case "BACKWARD":
+                        break;
+
+                    case "LEFT":
+                        break;
+
+                    case "RIGHT":
+                        break;
+                    case "LIGHT_ON":
+                        HttpGet("lights on");
+                        break;
+
+                    case "LIGHT_OFF":
+                        HttpGet("lights off");
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handler for rejected speech events.
+        /// </summary>
+        /// <param name="sender">object sending the event.</param>
+        /// <param name="e">event arguments.</param>
+        private void SpeechRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            ClearRecognitionHighlights();
+        }
+
+        #endregion Kinect Discovery & Setup
+
+        #region Properties
+
+        public static readonly DependencyProperty KinectSensorManagerProperty =
+            DependencyProperty.Register(
+                "KinectSensorManager",
+                typeof(KinectSensorManager),
+                typeof(MainWindow),
+                new PropertyMetadata(null));
+
+        public KinectSensorManager KinectSensorManager
+        {
+            get { return (KinectSensorManager)GetValue(KinectSensorManagerProperty); }
+            set { SetValue(KinectSensorManagerProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the last recognized gesture.
+        /// </summary>
+        private string _gesture;
+        public String Gesture
+        {
+            get { return _gesture; }
+
+            private set
+            {
+                if (_gesture == value)
+                    return;
+
+                _gesture = value;
+
+                Debug.WriteLine("Gesture = " + _gesture);
+
+                if (this.PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Gesture"));
+            }
+        }
+
+        #endregion Properties
+
+        #region Events
+
+        /// <summary>
+        /// Event implementing INotifyPropertyChanged interface.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion Events
+
+        #region Event Handlers
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e">Gesture event arguments.</param>
+        private void OnGestureRecognized(object sender, GestureEventArgs e)
+        {
+            Debug.WriteLine(e.GestureType);
+
+            switch (e.GestureType)
+            {
+                case GestureType.Menu:
+                    Gesture = "Menu";
+                    break;
+                case GestureType.WaveRight:
+                    Gesture = "Wave Right";
+                    break;
+                case GestureType.WaveLeft:
+                    Gesture = "Wave Left";
+                    break;
+                case GestureType.JoinedHands:
+                    Gesture = "Joined Hands";
+                    break;
+                case GestureType.SwipeLeft:
+                    Gesture = "Swipe Left";
+                    HttpGet("lights on");
+                    break;
+                case GestureType.SwipeRight:
+                    Gesture = "Swipe Right";
+                    HttpGet("lights off");
+
+                    break;
+                case GestureType.ZoomIn:
+                    Gesture = "Zoom In";
+                    break;
+                case GestureType.ZoomOut:
+                    Gesture = "Zoom Out";
+                    break;
+                default:
+                    break;
+            }
+            //HttpGet(Gesture);
+        }
+
+
+        private void HttpGet(String eventName)
+        {
+            string address = string.Format("http://localhost/testconnection.php?sender=kinect&&arg={0}", Uri.EscapeDataString(eventName));
+            string text;
+            using (WebClient client = new WebClient())
+            {
+                text = client.DownloadString(address);
+            }
+            Console.WriteLine(text);
+        }
+
+        private void HttpPost()
+        {
+            Console.WriteLine("testing");
+            // Create a request using a URL that can receive a post. 
+            WebRequest request = WebRequest.Create("http://www.contoso.com/PostAccepter.aspx ");
+            // Set the Method property of the request to POST.
+            request.Method = "POST";
+            // Create POST data and convert it to a byte array.
+            string postData = "This is a test that posts this string to a Web server.";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            // Set the ContentType property of the WebRequest.
+            request.ContentType = "application/x-www-form-urlencoded";
+            // Set the ContentLength property of the WebRequest.
+            request.ContentLength = byteArray.Length;
+            // Get the request stream.
+            Stream dataStream = request.GetRequestStream();
+            // Write the data to the request stream.
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            // Close the Stream object.
+            dataStream.Close();
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.
+            dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+            // Clean up the streams.
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (SkeletonFrame frame = e.OpenSkeletonFrame())
+            {
+                if (frame == null)
+                    return;
+
+                // resize the skeletons array if needed
+                if (skeletons.Length != frame.SkeletonArrayLength)
+                    skeletons = new Skeleton[frame.SkeletonArrayLength];
+
+                // get the skeleton data
+                frame.CopySkeletonDataTo(skeletons);
+
+                foreach (var skeleton in skeletons)
+                {
+                    // skip the skeleton if it is not being tracked
+                    if (skeleton.TrackingState != SkeletonTrackingState.Tracked)
+                        continue;
+
+                    // update the gesture controller
+                    gestureController.UpdateAllGestures(skeleton);
+                }
+            }
+        }
+
+        #endregion Event Handlers
+
+    }
+}
