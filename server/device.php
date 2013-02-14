ddaf0cf184509cb80af957c80a02c008d16d16be
haozhe <stan.lhz@gmail.com>
2013-02-14 00:06:32 -0500

    Add getDevices function, which get all information for all devices

--- server/device.php
+++ server/device.php
@@ -7,6 +7,7 @@ header("Content-type: application/json");
 switch ($_GET["f"]) {
     case "getDeviceById":getDeviceById($_GET["id"]);break;
     case "getDeviceList":getDeviceList();break;
+    case "getDevices":getDevices();break;
 
         break;
 
@@ -14,6 +15,7 @@ switch ($_GET["f"]) {
         break;
 }
 
+//Get the ids for all devices
 function getDeviceById($id) {
     $dbQuery = sprintf("SELECT * 
                         FROM device
@@ -25,7 +27,7 @@ function getDeviceById($id) {
     echo json_encode($result);
 }
 
-
+//Get detail information for a device
 function getDeviceList() {
     $dbQuery = sprintf("SELECT id 
                         FROM device WHERE 1
@@ -37,6 +39,21 @@ function getDeviceList() {
 
 
     echo json_encode($result);
+}
+
+//Get all information for all devices
+function getDevices(){
+    $dbQuery = sprintf("SELECT * 
+                        FROM device WHERE 1
+                        ");
+    
+    $result = getDBResultsArray($dbQuery);
+    
+    //$result = getDBResultRecord($dbQuery);
+
+
+    echo json_encode($result);
+    
 }
 
 
